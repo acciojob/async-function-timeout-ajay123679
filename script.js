8e0647858479ed6c22487f933313e408707bf396
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-10 10:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const a=document.getElementById("text");
+const b=document.getElementById("delay");
+const c=document.getElementById("output");
+const d=document.getElementById("btn");
+
+btn.addEventListener("click",fun);
+
+async function fun()
+{
+	await new Promise((resolve)=>{
+		setTimeout(()=>{
+			c.innerText=a.value;
+			resolve();
+		},b.value);
+	})
+}
